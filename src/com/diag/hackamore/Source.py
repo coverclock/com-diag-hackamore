9cda88616cd642fa8b2e72ff5e8c88bfb0146794
jsloan <jsloan@57787f05-46dc-4277-aca0-fea81b7b9c9c>
2014-02-15 23:56:14 +0000

    Checkpoint.
    git-svn-id: svn://graphite/hackamore/trunk/Hackamore@8 57787f05-46dc-4277-aca0-fea81b7b9c9c

--- src/com/diag/hackamore/Source.py
+++ src/com/diag/hackamore/Source.py
@@ -11,7 +11,10 @@ import select
 HOST = "127.0.0.1"
 PORT = 5038
 CONNECT = 0.0
-MULTIPLEX = 0.0
+SELECT = 0.0
+MAKEFILE = 1024
+READLINE = 512
+EOF = ""
 
 sources = { }
 
@@ -19,15 +22,27 @@ connections = { }
 
 sessions = { }
 
-def multiplex():
+def producer():
     for source in sources:
-        source.connect(CONNECT)
+        source.connect()
     for connection in connections:
         connection.authenticate()
-    for session in select.select(sessions.values(), None, None, MULTIPLEX)[0]:
-        pass
-    for connection in select.select(connections.values(), None, None, MULTIPLEX)[0]:
-        pass
+    for session in select.select(sessions.values(), None, None, SELECT)[0]:
+        session.read()
+    for connection in select.select(connections.values(), None, None, SELECT)[0]:
+        connection.read()
+
+def consumer():
+    for connection in connections:
+        if connection.queue.length() > 0:
+            datum = connection.queue[0]
+            del connection.queue[0]
+            yield ( connection.name, datum )
+    for session in sessions:
+        if session.queue.length() > 0:
+            datum = session.queue[0]
+            del session.queue[0]
+            yield ( session.name, datum )
 
 class Source:
     """
@@ -40,15 +55,15 @@ class Source:
     # BOOKKEEPING LAYER
     #
 
-    def __init__(self, name, host, port, username, secret, timeout):
-        self.socket = None
+    def __init__(self, name, host, port, username, secret):
         self.name = name
         self.host = host
         self.port = port
         self.username = username
         self.secret = secret
-        self.fileno = -1
-        self.error = None
+        self.socket = None
+        self.file = None
+        self.queue = [ ]
         sources[self.name] = self
         
     def __del__(self):
@@ -58,61 +73,95 @@ class Source:
             del sources[self.name]
 
     def __repr__(self):
-        return "Source(\"" + str(self.name) + "\",\"" + str(self.host) + "\"," + str(self.port) + ",\"" + str(self.username) + "\",\"" + str(self.secret) + "\"," + str(self.fileno) + ",\"" + str(self.error) + "," + str(self.authenticated) + "\")"
+        return "Source(\"" + str(self.name) + "," + str(self.host) + "," + str(self.port) + "," + str(self.username) + "," + ("authenticated" if self.name in sessions else "connected" if self.name in connections else "defined" if self.name in sources else "orphaned")  + ")"
         
     #
     # CONNECTION LAYER
     #
         
     def disconnect(self):
-        try:
-            self.socket.close()
-        except socket.error as self.error:
-            sys.stderr.write("Source.disconnect: socket.close() FAILED! error=\"" + str(self.error) + "\"\n")
-        else:
-            pass
-        finally:
-            self.socket = None
-            self.fileno = -1
-            if self.name in sessions:
-                del sessions[self.name]
-            if self.name in connections:
-                del connections[self.name]
-            sources[self.name] = self
+        if self.file != None:
+            try:
+                self.file.close()
+            except Exception as exception:
+                sys.stderr.write("Source.disconnect: file.close() failed! error=\"" + str(exception) + "\"\n")
+            else:
+                sys.stderr.write("Source: " + self.name + " closed.\n")
+            finally:
+                self.file = None
+        if self.socket != None:
+            try:
+                self.socket.close()
+            except Exception as exception:
+                sys.stderr.write("Source.disconnect: socket.close() failed! error=\"" + str(exception) + "\"\n")
+            else:
+                sys.stderr.write("Source: " + self.name + " disconnected.\n")
+            finally:
+                self.socket = None
+        self.queue.append(EOF)
+        if self.name in sessions:
+            del sessions[self.name]
+        if self.name in connections:
+            del connections[self.name]
+        sources[self.name] = self
 
-    def connect(self, timeout):
+    def open(self):
         try:
-            self.socket = socket.create_connection((self.host, self.port), timeout)
-        except socket.error as self.error:
-            sys.stderr.write("Source.connect: socket.create_connection() FAILED! error=\"" + str(self.error) + "\"\n")
-            self.socket = None
+            self.file = open(self.path, "r")
+        except Exception as exception:
+            sys.stderr.write("Source.open: file.open(\"" + self.path + "\") failed! \"" + str(exception) + "\"\n")
+            self.disconnect()
         else:
-            self.fileno = self.socket.fileno();
+            sys.stderr.write("Source: " + self.name + " opened.\n")
             del sources[self.name]
             connections[self.name] = self
         finally:
             pass
 
+    def connect(self):
+        try:
+            self.socket = socket.create_connection((self.host, self.port), CONNECT)
+        except Exception as exception:
+            sys.stderr.write("Source.connect: socket.create_connection((\"" + str(self.host) + "\"," + str(self.port) + ")) failed! \"" + str(exception) + "\"\n")
+            self.disconnect()
+        else:
+            sys.stderr.write("Source: " + self.name + " connected.\n")
+            try:
+                self.file = self.makefile("rwb", MAKEFILE)
+            except Exception as exception:
+                sys.stderr.write("Source.connect: socket.create_connection() failed! \"" + str(exception) + "\"\n")
+                self.disconnect()
+            else:
+                sys.stderr.write("Source: " + self.name + " opened.\n")
+                del sources[self.name]
+                connections[self.name] = self
+            finally:
+                pass
+        finally:
+            pass
+
     def fileno(self):
-        return self.fileno
+        return self.file.fileno() if True else self.socket.fileno()
     
     #
     # AUTHENTICATION LAYER
     #
-    
-    def authenticated(self):
-        del connections[self.name]
-        sessions[self.name] = self
 
-    def deauthenticated(self):
-        del sessions[self.name]
-        connections[self.name] = self
+    def disauthenticate(self):
+        self.write("Action: Logoff\r\n\r\n")
 
     def authenticate(self):
         self.write("Action: Login\r\nUsername: " + str(self.username) + "\r\nSecret: " + str(self.secret) + "\r\n\r\n")
 
-    def deauthenticate(self):
-        self.write("Action: Logoff\r\n\r\n")
+    def disauthenticated(self):
+        sys.stderr.write("Source: " + self.name + " disauthenticated.\n")
+        del sessions[self.name]
+        connections[self.name] = self
+    
+    def authenticated(self):
+        sys.stderr.write("Source: " + self.name + " authenticated.\n")
+        del connections[self.name]
+        sessions[self.name] = self
 
     #
     # INPUT/OUTPUT LAYER
@@ -120,23 +169,26 @@ class Source:
 
     def write(self, stuff):
         try:
-            self.socket.sendall(stuff)
-        except socket.error as self.error:
-            sys.stderr.write("Source.write: socket.sendall() FAILED! error=\"" + str(self.error) + "\"\n")
+            self.file.write(stuff)
+        except Exception as exception:
+            sys.stderr.write("Source.write: file.write() failed! \"" + str(exception) + "\"\n")
             self.disconnect()
         else:
-            self.error = None
+            self.file.flush()
         finally:
             pass
     
-    def read(self, limit):
+    def read(self):
         try:
-            data = self.socket.recv(limit)
-        except socket.error as self.error:
-            sys.stderr.write("Source.read: socket.recv() FAILED! error=\"" + str(self.error) + "\"\n")
+            line = self.file.readline(READLINE)
+        except Exception as exception:
+            sys.stderr.write("Source.read: file.readline() failed! \"" + str(exception) + "\"\n")
             self.disconnect()
-            data = ""
         else:
-            pass
+            if line.length() == 0:
+                self.disconnect()
+            else:
+                self.queue.append(line)
         finally:
-            return data
+            pass
+
