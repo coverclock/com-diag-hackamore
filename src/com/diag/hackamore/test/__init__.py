dd28106ee237dea030de43d0c0ed490edc32ce2f
jsloan <jsloan@57787f05-46dc-4277-aca0-fea81b7b9c9c>
2014-02-18 20:49:59 +0000

    Completed moving all UTs. All UTs pass.
    git-svn-id: svn://graphite/hackamore/trunk/Hackamore@25 57787f05-46dc-4277-aca0-fea81b7b9c9c

--- src/com/diag/hackamore/test/__init__.py
+++ src/com/diag/hackamore/test/__init__.py
@@ -1,5 +1,5 @@
 """
-@package com.diag.hackamore
+@package com.diag.hackamore.test
 Copyright 2014 by the Digital Aggregates Corporation, Colorado, USA.
 Licensed under the terms in the README.txt file.
 """
