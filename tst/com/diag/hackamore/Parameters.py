025837708f9897095525cfc7a7b7a01ab4c15bde
jsloan <jsloan@57787f05-46dc-4277-aca0-fea81b7b9c9c>
2014-02-28 17:09:58 +0000

    Converting asserts.
    git-svn-id: svn://graphite/hackamore/trunk/Hackamore@47 57787f05-46dc-4277-aca0-fea81b7b9c9c

--- tst/com/diag/hackamore/Parameters.py
+++ tst/com/diag/hackamore/Parameters.py
@@ -5,7 +5,9 @@ Licensed under the terms in the README.txt file.
 These are unit test parameters, some of which are likely to be specific to a
 particular installation. At the very least you will need to change SERVER,
 USERNAME, and SECRET to match your network and what you have administered in
-your /etc/asterisk/manager.conf file.
+your /etc/asterisk/manager.conf file. If you don't want to test against a live
+Asterisk server, set SERVER to an zero length string and the live test(s) will
+be bypassed.
 """
 
 SERVER = "192.168.1.220"
