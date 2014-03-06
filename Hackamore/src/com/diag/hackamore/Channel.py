c94e4ebc9b2a0f4afe3b8fbf60f415e33d8fcb11
jsloan <jsloan@57787f05-46dc-4277-aca0-fea81b7b9c9c>
2014-03-06 18:21:51 +0000

    Engine unit test runs but individual State and Channel UTs not done.
    git-svn-id: svn://graphite/hackamore/trunk@84 57787f05-46dc-4277-aca0-fea81b7b9c9c

--- Hackamore/src/com/diag/hackamore/Channel.py
+++ Hackamore/src/com/diag/hackamore/Channel.py
@@ -7,15 +7,17 @@ Licensed under the terms in the README.txt file.
 IDLE = 0
 CALLING = 1
 CALLED = 2
-BRIDGE = 3
+TRUNK = 3
 
-ROLE = ( "IDLE", "CALLING", "CALLED", "BRIDGE" )
+ROLE = ( "IDLE", "CALLING", "CALLED", "TRUNK" )
 
 class Channel():
 
     def __init__(self, pbx, uniqueid, channel, channelstate, channelstatedesc):
         self.pbx = pbx
         self.uniqueid = uniqueid
+        self.sipcallid = None
+        self.conference = None
         self.channel = channel
         self.channelstate = channelstate
         self.channelstatedesc = channelstatedesc
@@ -26,7 +28,7 @@ class Channel():
         pass
 
     def __repr__(self):
-        return "Channel(" + str(self.pbx) + "," + str(self.uniqueid) + "," + str(self.channel) + "," + str(self.channelstatedesc) + "," + str(ROLE[self.role]) + ")"
+        return "Channel(" + str(self.pbx) + "," + str(self.uniqueid) + "," + str(self.sipcallid) + "," + str(self.channel) + "," + str(self.conference) + "," + str(self.channelstatedesc) + "," + str(ROLE[self.role]) + ")"
 
     def newstate(self, channelstate, channelstatedesc):
         self.channelstate = channelstate
@@ -34,12 +36,18 @@ class Channel():
 
     def rename(self, channel):
         self.channel = channel
+        
+    def endpoint(self, sipcallid):
+        self.sipcallid = sipcallid
+        
+    def trunk(self):
+        self.role = TRUNK
 
     def calling(self):
         if self.role == IDLE:
             self.role = CALLING
         elif self.role == CALLED:
-            self.role = BRIDGE
+            self.role = TRUNK
         else:
             pass
     
@@ -47,6 +55,12 @@ class Channel():
         if self.role == IDLE:
             self.role = CALLED
         elif self.role == CALLING:
-            self.role = BRIDGE
+            self.role = TRUNK
         else:
             pass
+
+    def join(self, conference):
+        self.conference = conference
+
+    def leave(self, conference):
+        self.conference = None
