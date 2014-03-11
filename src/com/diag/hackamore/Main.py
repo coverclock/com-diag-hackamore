05ad81d49cd6d716a4bcd5636bcd919c57fb9f34
jsloan <jsloan@57787f05-46dc-4277-aca0-fea81b7b9c9c>
2014-03-11 21:59:07 +0000

    Implemented more general .com_diag_hackamore config file.
    git-svn-id: svn://graphite/hackamore/trunk/Hackamore@110 57787f05-46dc-4277-aca0-fea81b7b9c9c

--- src/com/diag/hackamore/Main.py
+++ src/com/diag/hackamore/Main.py
@@ -19,14 +19,35 @@ import com.diag.hackamore.Controller
 def main():
     logger = com.diag.hackamore.Logger.logger()
     sources = [ ]
-    #####
-    name1 = com.diag.hackamore.Credentials.credential(com.diag.hackamore.Credentials.COM_DIAG_HACKAMORE_SERVER)
-    server1 = com.diag.hackamore.Credentials.credential(com.diag.hackamore.Credentials.COM_DIAG_HACKAMORE_SERVER)
-    username1 = com.diag.hackamore.Credentials.credential(com.diag.hackamore.Credentials.COM_DIAG_HACKAMORE_USERNAME)
-    secret1 = com.diag.hackamore.Credentials.credential(com.diag.hackamore.Credentials.COM_DIAG_HACKAMORE_SECRET)
-    source1 = com.diag.hackamore.Socket.Socket(name1, username1, secret1, server1)
-    sources.append(source1)
-    #####
+    prefix = "COM_DIAG_HACKAMORE_"
+    index = 1
+    while True:
+        names = prefix + "NAME" + str(index)
+        name = com.diag.hackamore.Credentials.credential(names)
+        if not name:
+            break
+        servers = prefix + "SERVER" + str(index)
+        server = com.diag.hackamore.Credentials.credential(servers)
+        if not server:
+            server = com.diag.hackamore.Socket.HOST
+        ports = prefix + "PORT" + str(index)
+        port = com.diag.hackamore.Credentials.credential(ports)
+        if not port:
+            port = com.diag.hackamore.Socket.PORT
+        else:
+            port = int(port)
+        usernames = prefix + "USERNAME" + str(index)
+        username = com.diag.hackamore.Credentials.credential(usernames)
+        if not username:
+            break
+        secrets = prefix + "SECRET" + str(index)
+        secret = com.diag.hackamore.Credentials.credential(secrets)
+        if not secret:
+            break
+        logger.info("Main.main: %s=\"%s\" %s=\"%s\" %s=\"%s\" %s=\"%s\"", names, name, servers, server, usernames, username, secrets, secret)
+        source = com.diag.hackamore.Socket.Socket(name, username, secret, server, port)
+        sources.append(source)
+        index = index + 1
     model = com.diag.hackamore.ModelStandard.ModelStandard()
     mutex = threading.Condition()
     serializedmodel = com.diag.hackamore.ModelSerializer.ModelSerializer(model, mutex)
@@ -34,10 +55,11 @@ def main():
     serializedview = com.diag.hackamore.ViewSerializer.ViewSerializer(view, mutex)
     controller = com.diag.hackamore.Controller.Controller(serializedmodel, serializedview)
     logger.info("Main.main: STARTING.")
-    while True:
+    while sources:
         controller.loop(sources, sources)
         time.sleep(2.0)
         logger.info("Main.main: RESTARTING.")
+    logger.info("Main.main: STOPPING.")
 
 if __name__ == "__main__":
     main()
